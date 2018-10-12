9ee4dc55248d8cdf6d46700913b1d4a9f0a27082
ogbeniore <oreeboy@gmail.com>
2018-10-13 00:48:54 +0100

    profile edit page.. !fully responsive

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -5,141 +5,155 @@ class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
-        <div className="sidebar-cover-image-wrapper">
-          <img
-            className="sidebar-cover-image"
-            src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363181/gclout/Rectangle_2.1.png"
-            alt="cover image"
-          />
-        </div>
-        <div className="sidebar-section --lift-up">
-          <div className="sidebar-profile-image-wrapper">
+        <div className="sidebar-top">
+          <div className="sidebar-cover-image-wrapper">
             <img
-              className="sidebar-profile-image"
-              src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-              alt="profile image"
+              className="sidebar-cover-image"
+              src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363181/gclout/Rectangle_2.1.png"
+              alt="cover image"
             />
           </div>
-          <h5 className="text-center">James Adewale</h5>
-          <div className="d-flex sidebar-buttons justify-content-between">
-            <button className="btn btn-gclout-blue-outline">
-              Send Message
-            </button>
-            <button className="btn btn-gclout-blue">Friends</button>
-          </div>
-        </div>
-        <div className="sidebar-section">
-          <p className="text-gclout-blue">Contact Information</p>
-          <p>Email Address</p>
-          <p>jamesadewale@gmail.com</p>
-          <br />
-          <p>Phone Number</p>
-          <p>+234 [0] 802 345 6789</p>
-        </div>
-        <div className="sidebar-section">
-          <p className="text-gclout-blue">Other Information</p>
-          <div className="d-flex">
-            <div className="section-half">
-              <p>Nationality</p>
-              <p>Nigeria</p>
-              <br />
-              <p>L.G.A</p>
-              <p>Ikeja</p>
+          <div className="sidebar-section --lift-up">
+            <div className="sidebar-profile-image-wrapper">
+              <img
+                className="sidebar-profile-image"
+                src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                alt="profile image"
+              />
             </div>
-            <div className="section-half">
-              <p>State</p>
-              <p>Lagos State</p>
-              <br />
-              <p>Date of birth</p>
-              <p>23/07/1990</p>
+            <h5 className="text-center">James Adewale</h5>
+            <div className="d-flex sidebar-buttons justify-content-between">
+              <button className="btn btn-gclout-blue-outline">
+                Send Message
+              </button>
+              <button className="btn btn-gclout-blue">Friends</button>
             </div>
           </div>
-        </div>
-        <div className="sidebar-section --no-border">
-          <p className="text-gclout-blue">Political Office Holders</p>
-          <div className="d-flex">
-            <div className="section-half">
-              <div className="text-center sidebar-politician">
-                <div className="politician-profile-image-wrapper">
-                  <img
-                    className="politician-profile-image"
-                    src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-                    alt="friend profile image"
-                  />
-                </div>
-                <p>Marks Webber</p>
-                <small>President</small>
+          <div className="sidebar-section">
+            <p className="text-gclout-blue">Contact Information</p>
+            <p>Email Address</p>
+            <p>jamesadewale@gmail.com</p>
+            <br />
+            <p>Phone Number</p>
+            <p>+234 [0] 802 345 6789</p>
+          </div>
+          <div className="sidebar-section">
+            <p className="text-gclout-blue">Other Information</p>
+            <div className="d-flex">
+              <div className="section-half">
+                <p>Nationality</p>
+                <p>Nigeria</p>
+                <br />
+                <p>L.G.A</p>
+                <p>Ikeja</p>
               </div>
-              <div className="text-center sidebar-politician">
-                <div className="politician-profile-image-wrapper">
-                  <img
-                    className="politician-profile-image"
-                    src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-                    alt="friend profile image"
-                  />
-                </div>
-                <p>Marks Webber</p>
-                <small>Council chairman</small>
-              </div>
-              <div className="text-center sidebar-politician">
-                <div className="politician-profile-image-wrapper">
-                  <img
-                    className="politician-profile-image"
-                    src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-                    alt="friend profile image"
-                  />
-                </div>
-                <p>Marks Webber</p>
-                <small>Federal Rep</small>
-              </div>
-              <div className="text-center sidebar-politician">
-                <div className="politician-profile-image-wrapper">
-                  <img
-                    className="politician-profile-image"
-                    src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-                    alt="friend profile image"
-                  />
-                </div>
-                <p>Marks Webber</p>
-                <small>Councilor</small>
-              </div>
-            </div>
-            <div className="section-half">
-              <div className="text-center sidebar-politician">
-                <div className="politician-profile-image-wrapper">
-                  <img
-                    className="politician-profile-image"
-                    src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-                    alt="friend profile image"
-                  />
-                </div>
-                <p>Marks Webber</p>
-                <small>Governor</small>
-              </div>
-              <div className="text-center sidebar-politician">
-                <div className="politician-profile-image-wrapper">
-                  <img
-                    className="politician-profile-image"
-                    src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-                    alt="friend profile image"
-                  />
-                </div>
-                <p>Marks Webber</p>
-                <small>Senator</small>
-              </div>
-              <div className="text-center sidebar-politician">
-                <div className="politician-profile-image-wrapper">
-                  <img
-                    className="politician-profile-image"
-                    src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
-                    alt="friend profile image"
-                  />
-                </div>
-                <p>Marks Webber</p>
-                <small>State rep</small>
+              <div className="section-half">
+                <p>State</p>
+                <p>Lagos State</p>
+                <br />
+                <p>Date of birth</p>
+                <p>23/07/1990</p>
               </div>
             </div>
           </div>
+          <div className="sidebar-section --no-border">
+            <p className="text-gclout-blue">Political Office Holders</p>
+            <div className="d-flex">
+              <div className="section-half">
+                <div className="text-center sidebar-politician">
+                  <div className="politician-profile-image-wrapper">
+                    <img
+                      className="politician-profile-image"
+                      src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                      alt="friend profile image"
+                    />
+                  </div>
+                  <p>Marks Webber</p>
+                  <small>President</small>
+                </div>
+                <div className="text-center sidebar-politician">
+                  <div className="politician-profile-image-wrapper">
+                    <img
+                      className="politician-profile-image"
+                      src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                      alt="friend profile image"
+                    />
+                  </div>
+                  <p>Marks Webber</p>
+                  <small>Council chairman</small>
+                </div>
+                <div className="text-center sidebar-politician">
+                  <div className="politician-profile-image-wrapper">
+                    <img
+                      className="politician-profile-image"
+                      src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                      alt="friend profile image"
+                    />
+                  </div>
+                  <p>Marks Webber</p>
+                  <small>Federal Rep</small>
+                </div>
+                <div className="text-center sidebar-politician">
+                  <div className="politician-profile-image-wrapper">
+                    <img
+                      className="politician-profile-image"
+                      src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                      alt="friend profile image"
+                    />
+                  </div>
+                  <p>Marks Webber</p>
+                  <small>Councilor</small>
+                </div>
+              </div>
+              <div className="section-half">
+                <div className="text-center sidebar-politician">
+                  <div className="politician-profile-image-wrapper">
+                    <img
+                      className="politician-profile-image"
+                      src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                      alt="friend profile image"
+                    />
+                  </div>
+                  <p>Marks Webber</p>
+                  <small>Governor</small>
+                </div>
+                <div className="text-center sidebar-politician">
+                  <div className="politician-profile-image-wrapper">
+                    <img
+                      className="politician-profile-image"
+                      src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                      alt="friend profile image"
+                    />
+                  </div>
+                  <p>Marks Webber</p>
+                  <small>Senator</small>
+                </div>
+                <div className="text-center sidebar-politician">
+                  <div className="politician-profile-image-wrapper">
+                    <img
+                      className="politician-profile-image"
+                      src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                      alt="friend profile image"
+                    />
+                  </div>
+                  <p>Marks Webber</p>
+                  <small>State rep</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="sidebar-footer">
+          <p>&copy; {(new Date()).getFullYear()} GClout | All copyrights rights reserved</p>
+          <p>
+            <a href="#">Help &FAQ</a> | <a href="#">Terms Of Use</a> |{" "}
+            <a href="#">Privacy Policy</a>
+          </p>
+          <p className="smaller-margin">
+            <strong>Contact Us</strong>
+          </p>
+          <p  className="smaller-margin">Email: gclout@gmail.com</p>
+          <p  className="smaller-margin">Phone: 080 3333 3333</p>
         </div>
       </div>
     );
--- src/pages/profile/editProfile.jsx
+++ src/pages/profile/editProfile.jsx
@@ -2,15 +2,98 @@ import React, { Component } from "react";
 import NavBarAuthenticated from "../../components/navbar/navBarAuthenticated";
 import Sidebar from "../../components/sidebar/sidebar";
 import Footer from "../../components/footer/footer";
+import "../../assets/css/profile.css";
 
 class EditProfile extends Component {
   render() {
     return (
       <div className="app-wrapper">
         <NavBarAuthenticated />
-        <div className="container app-container">
-          <h4>My Profile</h4>
+        <div className="container app-container d-md-flex">
           <Sidebar />
+          <div className="app-content">
+            <h4 className="page-title">Edit your profile</h4>
+            <div className="profile-cover-image-wrapper">
+              <img
+                className="profile-cover-image"
+                src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363181/gclout/Rectangle_2.1.png"
+                alt="cover"
+              />
+              <button className="floating-edit-button-wrapper">
+                <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0.875 16.9032V21.122H5.09375L17.5362 8.67945L13.3175 4.4607L0.875 16.9032ZM20.7987 5.41695C21.2375 4.9782 21.2375 4.26945 20.7987 3.8307L18.1663 1.1982C17.7275 0.759453 17.0187 0.759453 16.58 1.1982L14.5212 3.25695L18.74 7.4757L20.7987 5.41695Z" fill="white"/>
+                </svg>
+              </button>
+            </div>
+            <div className="lifted-profile-image-wrapper">
+              <img
+                className="lifted-profile-image"
+                src="https://res.cloudinary.com/plushdeveloper/image/upload/v1539363398/gclout/Ellipse_1.png"
+                alt="profile image"
+              />
+              <button className="floating-edit-button-wrapper --profile-picture">
+                <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0.875 16.9032V21.122H5.09375L17.5362 8.67945L13.3175 4.4607L0.875 16.9032ZM20.7987 5.41695C21.2375 4.9782 21.2375 4.26945 20.7987 3.8307L18.1663 1.1982C17.7275 0.759453 17.0187 0.759453 16.58 1.1982L14.5212 3.25695L18.74 7.4757L20.7987 5.41695Z" fill="white"/>
+                </svg>
+              </button>
+            </div>
+            <div className="col-md-9 mx-auto">
+              <form>
+                <div className="form-row">
+                  <div className="form-group col" style={{ paddingLeft: "0", paddingRight: "10px"}}>
+                    <label htmlFor="Fname">First Name</label>
+                    <input name="Fname" className="form-control" type="text" placeholder="John" required />
+                  </div>
+                  <div className="form-group col" style={{ paddingRight: "0", paddingLeft: "10px"}}>
+                    <label htmlFor="Lname">Last Name</label>
+                    <input name="Lname" className="form-control" type="text" placeholder="Doe" required />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email Address</label>
+                  <input name="email" className="form-control" type="email" placeholder="johndoe@gclout.com" required />
+                </div>
+                <div className="form-row">
+                  <div className="form-group col" style={{ paddingLeft: "0", paddingRight: "10px"}}>
+                    <label htmlFor="phone">Phone Number</label>
+                    <input name="phone" className="form-control" type="phone" placeholder="+234 [0] 802 345 6789" required />
+                  </div>
+                  <div className="form-group col" style={{ paddingRight: "0", paddingLeft: "10px"}}>
+                    <label htmlFor="nationality">Nationality</label>
+                    <input name="nationality" className="form-control" type="text" placeholder="Nigerian" required />
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col" style={{ paddingLeft: "0", paddingRight: "10px"}}>
+                    <label htmlFor="state">State of residence/interest</label>
+                    <select name="state" className="form-control" required>
+                    <option value="lag">Lagos</option>
+                    <option value="ogun">Ogun</option>
+                    <option value="osun">Osun</option>
+                    </select>
+                  </div>
+                  <div className="form-group col" style={{ paddingRight: "0", paddingLeft: "10px"}}>
+                    <label htmlFor="lga">L.G.A</label>
+                    <input name="lga" className="form-control" type="text" placeholder="Kosofe" required />
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="form-group col" style={{ paddingLeft: "0", paddingRight: "10px"}}>
+                    <label htmlFor="password">Password</label>
+                    <input name="password" className="form-control" type="password" required />
+                  </div>
+                  <div className="form-group col" style={{ paddingRight: "0", paddingLeft: "10px"}}>
+                    <label htmlFor="cPassword">Confirm Password</label>
+                    <input name="cPassword" className="form-control" type="password" required />
+                  </div>
+                </div>
+                <div className="d-flex">
+                  <button className="btn btn-gclout-dark" type="button" style={{marginRight: "10px"}}>Cancel</button>
+                  <button className="btn btn-gclout-blue" type="submit" style={{marginLeft: "10px"}}>Save</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
         <Footer />
       </div>
