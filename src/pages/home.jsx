0a515df8d55d14fc1003db3cf04b16a1b0d81eee
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-10-29 18:17:00 +0000

    home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,78 @@
 import React, { Component } from "react";
 import logo from "../assets/images/logo.png";
-import { Redirect } from "react-router-dom";
+import NavBar from "../components/navbar/navBar"
+import {Redirect} from 'react-router-dom';
 
 class Home extends Component {
+
+  constructor(props) {
+    
+    super(props);
+    
+    this.state = {
+
+      redirect: false,
+
+    };
+
+    this.logout = this.logout.bind(this);
+  
+  }
+
+  componentWillMount() {
+
+    console.log(sessionStorage.getItem('uuid')); 
+    console.log(sessionStorage.getItem('token'));
+  
+    if(sessionStorage.getItem("data")){
+
+      console.log('d is here');
+
+    } else {
+
+      this.setState({
+        
+        redirect: true
+
+      })
+
+    }
+
+  }
+  
+  logout(){
+
+    sessionStorage.setItem('data', '');
+    sessionStorage.clear();
+    this.setState({
+
+      redirect: true
+
+    })
+
+  }
+
   render() {
-    return !this.props.isLoggedIn ? (
-      <Redirect to="/login" />
-    ) : (
+
+    if(this.state.redirect){
+
+      return (<Redirect to={'/login'}/>)
+    
+    }
+
+    return (
+      <div>
+      <NavBar />
+        
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Click on get started to start</p>
         </header>
+
+        <button type='button' className="button" onClick={this.logout}>Logout</button>
+
+        </div>
       </div>
     );
   }
