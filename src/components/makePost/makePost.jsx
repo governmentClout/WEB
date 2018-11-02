10dfe1ba2a6a9b95d678056502cbf2605d868e82
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-11-02 13:38:23 +0000

    redirect user to same page after making post

--- src/components/makePost/makePost.jsx
+++ src/components/makePost/makePost.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./makePost.css";
 import PostMedia from "./postMedia";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
+
 
 class MakePost extends Component {
   constructor(props) {
@@ -107,83 +109,89 @@ class MakePost extends Component {
 export default MakePost;
 
 class PostCreation extends Component {
+  
   constructor(props) {
+    
     super(props);
 
     this.state = {
+      
       wordCount: 0,
       post: "",
-      uploadImages: false
+      uploadImages: false,
+      toProfile: false
+    
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
   }
 
-  dataChange(ev) {
+  onChange(e) {
+    
     this.setState({
-      [ev.target.name]: ev.target.value
+    
+      [e.target.name]: e.target.value
+    
     });
   }
 
-  postData(ev) {
+  onSubmit(e) {
+
     const id = sessionStorage.getItem("uuid"),
       token = sessionStorage.getItem("token");
 
     console.log(id);
     console.log(token);
 
-    ev.preventDefault();
-
-    const post = this.state.post;
+    e.preventDefault();
 
     const data = {
-      post
-    };
+
+      post: this.state.post
+    
+    }
 
     console.log(data);
     console.log("lmao");
 
-    /*     const url = "http://api.gclout.com:3000/posts"; */
-
-    /* if(this.state.post){ */
-
-    // fetch('http://api.gclout.com:3000/posts', {
-    // method: "post",
-    // body: JSON.stringify({
-    //   post: this.state.post
-    //   }),
-    // headers: {
-    //   "Content-Type": "application/json",
-    //   "token": token,
-    //   "uuid": id
-    // }
-    // }).then(function(response){
-    //   console.log(response);
-    // }).then(function(body){
-    //   console.log(body);
-    // });
-
-    const header = {
-
-      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-      "Access-Control-Allow-Origin": "*",
-      token: token,
-      uuid: id
-
-    }
-
-    console.log(header);
-
     axios({
+    
       method: "post",
       url: "http://api.gclout.com:3000/posts",
       data: data,
       headers: {
+    
         "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
         token: token,
         uuid: id
+    
       }
+    
     })
       .then(response => {
-        console.log(response);
+
+        if (response.data.Success) {
+
+          this.setState({
+
+            toProfile: true
+
+          });
+
+          console.log('success');
+
+          sessionStorage.setItem("message", response.data.Success)
+        
+        } else {
+
+          console.log("login error")
+        
+        }
+
+        console.log(response.data.Success);
+
       })
       .catch(err => console.log(err));
 
@@ -209,6 +217,13 @@ class PostCreation extends Component {
   };
 
   render() {
+
+    if(this.state.toProfile === true) {
+
+      return <Redirect to="/profile" />
+    
+    }
+
     return (
       <div
         className={
@@ -217,7 +232,7 @@ class PostCreation extends Component {
       >
         <div className="pt-4 px-4 pb-5">
           <h5>Post</h5>
-          <form onSubmit={this.postData.bind(this)}>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <textarea
                 className={
@@ -226,10 +241,10 @@ class PostCreation extends Component {
                     : "form-control border-red"
                 }
                 rows="4"
-                name="new_post"
+                name="post"
                 onChange={this.updateWordCount}
                 value={this.state.post}
-                onFormChange={this.dataChange.bind(this)}
+                onChange={this.onChange}
                 placeholder="Type post here..."
               />
             </div>
