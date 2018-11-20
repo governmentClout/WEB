6f5726bf14e23e1ae85cbb665394554452ed8546
ogbeniore <oreeboy@gmail.com>
2018-11-20 20:11:57 +0100

    added build folder

---
+++ build/precache-manifest.52a09963f84a6ca3f1509e8864133b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3000b6e62f9e49b35d52a3459030260f",
+    "url": "/static/media/logo.3000b6e6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b3c28a009d62459b5941",
+    "url": "/static/js/main.b3c28a00.chunk.js"
+  },
+  {
+    "revision": "c2ade240f0361021c900",
+    "url": "/static/js/1.c2ade240.chunk.js"
+  },
+  {
+    "revision": "b3c28a009d62459b5941",
+    "url": "/static/css/main.37d39d59.chunk.css"
+  },
+  {
+    "revision": "c2ade240f0361021c900",
+    "url": "/static/css/1.361f1653.chunk.css"
+  },
+  {
+    "revision": "ce5444aa1ce47b713ee4f185b72cf01f",
+    "url": "/index.html"
+  }
+];
