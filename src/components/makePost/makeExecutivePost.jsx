6689b72825deec25b5687eb27990daa09a725ee8
ogbeniore <oreeboy@gmail.com>
2018-11-23 21:37:46 +0100

    executive profile done

--- src/components/makePost/makeExecutivePost.jsx
+++ src/components/makePost/makeExecutivePost.jsx
@@ -2,20 +2,24 @@ import React, { Component } from "react";
 import "./makePost.css";
 import PostMedia from "./postMedia";
 import axios from "axios";
-import { Redirect } from "react-router-dom";
+import { Manager, Reference, Popper } from "react-popper";
 
-
-class MakePost extends Component {
+class MakeExecutivePost extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showNewPost: false
+      showNewPost: false,
+      showFilters: false
     };
     this.newPostToggle = this.newPostToggle.bind(this);
   }
   newPostToggle() {
     let currentState = this.state.showNewPost;
     this.setState({ showNewPost: !currentState });
+  }
+  showFilters = () => {
+    let currentState = this.state.showFilters;
+    this.setState({ showFilters: !currentState });
   }
   updatePosts = () => this.props.updatePosts();
   render() {
@@ -29,9 +33,66 @@ class MakePost extends Component {
             />
             Post
           </button>
-          <p className="text-right ml-auto align-self-center">
-            Filters
-          </p>
+          <div className="ml-auto align-self-center">
+            <Manager>
+              <Reference>
+                {({ ref }) => (
+                  <button
+                    type="button"
+                    ref={ref}
+                    onClick={this.showFilters}
+                    className="btn btn-white"
+                  >
+                  <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 12H8C7.45 12 7 11.55 7 11C7 10.45 7.45 10 8 10H10C10.55 10 11 10.45 11 11C11 11.55 10.55 12 10 12ZM18 1C18 1.55 17.55 2 17 2H1C0.450001 2 0 1.55 0 1C0 0.45 0.450001 0 1 0H17C17.55 0 18 0.45 18 1ZM14 7H4C3.45 7 3 6.55 3 6C3 5.45 3.45 5 4 5H14C14.55 5 15 5.45 15 6C15 6.55 14.55 7 14 7Z" fill="#333333"/>
+                  </svg> {" "}
+                    Filters
+                  </button>
+                )}
+              </Reference>
+              <Popper placement="bottom">
+                {({ ref, style, placement, arrowProps }) => (
+                  <div ref={ref} style={style} data-placement={placement}>
+                    <div
+                      className={
+                        this.state.showFilters
+                          ? "button-dropdown show"
+                          : "button-dropdown"
+                      }
+                    >
+                      <ul className="button-dropdown-list">
+                        <li
+                          className="profile-dropdown-list-item"
+                          onClick={this.showFilters}
+                        >
+                          <button className="button-dropdown-list-button">All</button>
+                        </li>
+                        <li
+                          className="profile-dropdown-list-item"
+                          onClick={this.showFilters}
+                        >
+                          <button className="button-dropdown-list-button">Ministry</button>
+                        </li>
+                        <li
+                          className="profile-dropdown-list-item"
+                          onClick={this.showFilters}
+                        >
+                          <button className="button-dropdown-list-button">Department</button>
+                        </li>
+                        <li
+                          className="profile-dropdown-list-item"
+                          onClick={this.showFilters}
+                        >
+                          <button className="button-dropdown-list-button">Agency</button>
+                        </li>
+                      </ul>
+                    </div>
+                    <div ref={arrowProps.ref} style={arrowProps.style} />
+                  </div>
+                )}
+              </Popper>
+            </Manager> 
+          </div>
         </div>
         <PostCreation updatePosts={this.updatePosts} show={this.state.showNewPost} />
       </div>
@@ -39,7 +100,7 @@ class MakePost extends Component {
   }
 }
 
-export default MakePost;
+export default MakeExecutivePost;
 
 class PostCreation extends Component {
   
@@ -51,11 +112,24 @@ class PostCreation extends Component {
       uploadImages: false,
       toProfile: false,
       disable: false,
-      loading: false
+      loading: false,
+      class: '',
+      department: ''
+
     };
 
     this.updateWordCount= this.updateWordCount.bind(this);
     this.onSubmit = this.onSubmit.bind(this)
+  }
+  
+  handleChange = (e) => {
+
+    this.setState({
+
+      [e.target.name]: e.target.value
+
+    });
+  
   }
   updatePostsNow = () => this.props.updatePosts()
   onSubmit(e) {
@@ -139,8 +213,37 @@ class PostCreation extends Component {
         }
       >
         <div className="pt-4 px-4 pb-5">
-          <h5>Post</h5>
           <form onSubmit={this.onSubmit}>
+          <div className="form-row">
+            <div className="form-group col-md">
+                <label htmlFor="class">Pick a class</label>
+                <select
+                  name="class"
+                  className="form-control"
+                  onChange={this.handleChange}
+                  required
+                >
+                  <option value="1">Class one</option>
+                  <option value="2">Class two</option>
+                  <option value="3">Class three</option>
+                  <option value="4">Class four</option>
+                </select>
+              </div>
+              <div className="form-group col-md">
+                <label htmlFor="department">State</label>
+                 <select
+                  name="department"
+                  className="form-control"
+                  onChange={this.handleChange}
+                  required
+                >
+                  <option value="ministry">Ministry</option>
+                  <option value="department">Department</option>
+                  <option value="agency">Agency</option>
+                </select>
+              </div>
+            </div>
+            <h6>Post</h6>
             <div className="form-group">
               <textarea
                 className={
