6efe0f5261e149523899c4019bc3754e348882c5
ogbeniore <oreeboy@gmail.com>
2018-12-04 20:30:08 +0100

    added build v4

---
+++ build/precache-manifest.f1b6c4e9acde86f6296ea4695c74ce9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3000b6e62f9e49b35d52a3459030260f",
+    "url": "/static/media/logo.3000b6e6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c50ec4a6861478bc29dc",
+    "url": "/static/js/main.c50ec4a6.chunk.js"
+  },
+  {
+    "revision": "0d295ecd98b84c238c3b",
+    "url": "/static/js/1.0d295ecd.chunk.js"
+  },
+  {
+    "revision": "c50ec4a6861478bc29dc",
+    "url": "/static/css/main.6d6217ab.chunk.css"
+  },
+  {
+    "revision": "0d295ecd98b84c238c3b",
+    "url": "/static/css/1.04308288.chunk.css"
+  },
+  {
+    "revision": "12353dc32a6d97260ccbff5c0ece3616",
+    "url": "/index.html"
+  }
+];
