61fd0c2594d5e0a5bf3fb30aa83c87b126a00eaa
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-12-04 12:21:26 +0000

    clean

--- src/pages/profile/friends.jsx
+++ src/pages/profile/friends.jsx
@@ -3,27 +3,25 @@ import "../../assets/css/pages.css";
 import Suggestions from "../../components/suggestions/suggestions";
 import Trending from "../../components/trending/trending";
 import Sidebar from "../../components/sidebar/sidebar";
-import Friend from "../../components/friend/friend"
-import { Redirect } from "react-router-dom"
 import axios from "axios";
+import { Manager, Reference, Popper } from "react-popper";
 
 class FriendsPage extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
-
+            showMenu: false,
             friends: []
-
-        }
-
+        };
+        this.showMenu = this.showMenu.bind(this);
     }
-
-    componentDidMount() {
-
+    showMenu() {
+        let currentState = this.state.showMenu;
+        this.setState({ showMenu: !currentState });
+    }
+    componentWillMount() {
         this.fetchFriends();
-
     }
 
     fetchFriends(){
@@ -73,7 +71,69 @@ class FriendsPage extends Component {
                   </form>
                 </div>
                 <div className="friends-list">
-                  {this.state.friends.map((friend) => (<Friend friend={friend}/>))}
+                  {this.state.friends.map(
+                      /*(friend) => (<Friend friend={friend}/>)*/
+                      (friend) => (
+                          <div className="friend-details-container">
+                              <div className="friend-details">
+                                  <div className="friend-image-container  my-auto">
+                                      <img
+                                          src="https://res.cloudinary.com/plushdeveloper/image/upload/v1540898186/profile_eyjfnd.jpg"
+                                          className="friend-image"
+                                          alt="friend"
+                                      />
+                                  </div>
+                                  <div className="deats  my-auto">
+                                      <h5>{friend.firstName + " " + friend.lastName}</h5>
+                                      <p>Lagos, Nigeria</p>
+                                  </div>
+                              </div>
+                              <div className="friend-action">
+                                  {this.props.type !== "suggested" ? (
+                                      <>
+                                          <button className="btn btn-gclout-blue-outline">Message</button>
+                                          <Manager>
+                                              <Reference>
+                                                  {({ ref }) => (
+                                                      <button
+                                                          className="btn-icon"
+                                                          ref={ref}
+                                                          onClick={this.showMenu}
+                                                      >
+                                                          <i className="fas fa-ellipsis-v" />
+                                                      </button>
+                                                  )}
+                                              </Reference>
+                                              <Popper placement="bottom">
+                                                  {({ ref, style, placement, arrowProps }) => (
+                                                      <div ref={ref} style={style} data-placement={placement}>
+                                                          <div
+                                                              className={
+                                                                  this.state.showMenu ? "side-drop show" : "side-drop"
+                                                              }
+                                                          >
+                                                              <ul className="side-drop-list">
+                                                                  <li className="side-drop-list-item">
+                                                                      <button className="side-drop-button">Remove</button>
+                                                                  </li>
+                                                                  <li className="side-drop-list-item">
+                                                                      <button className="side-drop-button">Block</button>
+                                                                  </li>
+                                                              </ul>
+                                                          </div>
+                                                          <div ref={arrowProps.ref} style={arrowProps.style} />
+                                                      </div>
+                                                  )}
+                                              </Popper>
+                                          </Manager>
+                                      </>
+                                  ) : (
+                                      <button className="btn btn-gclout-blue-outline">Add Friend</button>
+                                  )}
+                              </div>
+                          </div>
+                      )
+                  )}
                 </div>
               </div>
             </div>
