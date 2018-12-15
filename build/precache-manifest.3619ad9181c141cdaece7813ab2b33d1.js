724a42f1efa6264983d9e193da6529bc066812d8
ogbeniore <oreeboy@gmail.com>
2018-12-15 18:04:13 +0100

    built files

---
+++ build/precache-manifest.3619ad9181c141cdaece7813ab2b33d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3000b6e62f9e49b35d52a3459030260f",
+    "url": "/static/media/logo.3000b6e6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13319a5049c90a7bf5d5",
+    "url": "/static/js/main.13319a50.chunk.js"
+  },
+  {
+    "revision": "40714309fef8136bd3d5",
+    "url": "/static/js/1.40714309.chunk.js"
+  },
+  {
+    "revision": "13319a5049c90a7bf5d5",
+    "url": "/static/css/main.0c5654d3.chunk.css"
+  },
+  {
+    "revision": "40714309fef8136bd3d5",
+    "url": "/static/css/1.bdc8d850.chunk.css"
+  },
+  {
+    "revision": "2e27b4932dbf4ce03d7d027ebfcb35fa",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -16,6 +16,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox
 importScripts(
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   "/precache-manifest.028c31b974c682466451befd3ec29c15.js"
 =======
   "/precache-manifest.f1b6c4e9acde86f6296ea4695c74ce9c.js"
@@ -23,6 +24,9 @@ importScripts(
 =======
   "/precache-manifest.028c31b974c682466451befd3ec29c15.js"
 >>>>>>> adde build v5
+=======
+  "/precache-manifest.3619ad9181c141cdaece7813ab2b33d1.js"
+>>>>>>> built files
 );
 
 workbox.clientsClaim();
