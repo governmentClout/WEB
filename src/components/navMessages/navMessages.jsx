55751c9870476781d7f35a50dcf04be658a5a1f3
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-12-22 18:43:56 +0000

    hook up messages

--- src/components/navMessages/navMessages.jsx
+++ src/components/navMessages/navMessages.jsx
@@ -2,8 +2,61 @@ import React, { Component } from "react";
 import "./navMessages.css";
 import {Link} from "react-router-dom";
 import Reply from "@material-ui/icons/Reply"
+import {API_URL} from "../config";
+import axios from 'axios';
 
 class navMessagees extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+      messages: [],
+      loading: false
+
+    }
+
+  }
+
+  componentDidMount() {
+
+    this.getMessages();
+
+  }
+
+  getMessages(){
+
+    this.setState({
+      loading: true
+
+    });
+
+    const id = sessionStorage.getItem("uuid"),
+        token = sessionStorage.getItem("token");
+
+    axios({
+
+      method: 'get',
+      url: `${API_URL}/messages`,
+      headers: {
+
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+        token: token,
+        uuid: id
+
+      }
+
+    }).then(res => {
+
+      console.log(res.data);
+      console.log('here');
+
+    })
+
+  }
+
+
   render() {
     return (
       <React.Fragment>
