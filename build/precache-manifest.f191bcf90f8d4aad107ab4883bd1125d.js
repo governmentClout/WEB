76a312efaee81f923e62509ca078dcc120e136a2
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-12-27 15:01:41 +0000

    build for deploy

---
+++ build/precache-manifest.f191bcf90f8d4aad107ab4883bd1125d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3000b6e62f9e49b35d52a3459030260f",
+    "url": "/static/media/logo.3000b6e6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "944b11c979122179391b",
+    "url": "/static/js/main.944b11c9.chunk.js"
+  },
+  {
+    "revision": "e19140b923ed5bbe7114",
+    "url": "/static/js/1.e19140b9.chunk.js"
+  },
+  {
+    "revision": "944b11c979122179391b",
+    "url": "/static/css/main.b7953117.chunk.css"
+  },
+  {
+    "revision": "e19140b923ed5bbe7114",
+    "url": "/static/css/1.14ab401f.chunk.css"
+  },
+  {
+    "revision": "73f9ce2723ad98d4e1bf131f42cd60c4",
+    "url": "/index.html"
+  }
+];
