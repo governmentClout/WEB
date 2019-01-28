dacbba8466af9c8ca683f5965bd0715ab7e753f5
ogbeniore <oreeboy@gmail.com>
2019-01-28 09:09:35 +0100

    build with 30% fixes on UI

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7f2aba61b939c66cd0ca681e6d71138.js"
+  "/precache-manifest.70b5c290a367f6bfdc455a27024f1ba6.js"
 );
 
 workbox.clientsClaim();
