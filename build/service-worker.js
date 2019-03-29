a98589201b1d2fc7fead7ebf669fc7d608eda401
Everistus Olumese <everistusolumese@gmail.com>
2019-03-29 03:31:26 +0100

    Alignment and Spacing #63

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83ab865d5b116670ce6547ed5d410cc5.js"
+  "/precache-manifest.07f28814b83e1e5e98193324fe5176dd.js"
 );
 
 workbox.clientsClaim();
