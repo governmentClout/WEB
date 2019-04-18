adbc319cf54fb661dd3d09f55b7dbd04a093cffe
Seyi Onifade <xyluz@ymail.com>
2019-04-18 23:16:36 +0100

    getting started understanding the project

---
+++ build/precache-manifest.f2224cc37f08059ddf76834347036565.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3000b6e62f9e49b35d52a3459030260f",
+    "url": "/static/media/logo.3000b6e6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c847e8ed47850aaa6a0c",
+    "url": "/static/js/main.c847e8ed.chunk.js"
+  },
+  {
+    "revision": "890b7c8b6b3a41f181b4",
+    "url": "/static/js/1.890b7c8b.chunk.js"
+  },
+  {
+    "revision": "c847e8ed47850aaa6a0c",
+    "url": "/static/css/main.d4f9201e.chunk.css"
+  },
+  {
+    "revision": "890b7c8b6b3a41f181b4",
+    "url": "/static/css/1.b4972fdc.chunk.css"
+  },
+  {
+    "revision": "c1732ccec792205df678fb8ed173b58f",
+    "url": "/index.html"
+  }
+];
--- src/components/config.js
+++ src/components/config.js
@@ -1,3 +1,5 @@
+//check if its local or online and connect to endpoint based on that
 export const API_URL = 'http://api.gclout.com:3000';
+
 export const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/xyluz/image/upload';
 export const CLOUDINARY_UPLOAD_PRESET = 'bbhcijzf';
